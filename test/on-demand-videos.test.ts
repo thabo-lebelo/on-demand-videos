3641e382f41f2f14ce919d018b6463cbd22d8d26
Thabo Lebelo <thabolebelo@icloud.com>
2022-06-17 19:17:16 +0200

    Initialize CDK application

---
+++ test/on-demand-videos.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as OnDemandVideos from '../lib/on-demand-videos-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/on-demand-videos-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new OnDemandVideos.OnDemandVideosStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
