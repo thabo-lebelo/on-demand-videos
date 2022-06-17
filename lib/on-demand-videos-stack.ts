0de0a078d90d8714ba5fa043cb27d7dec09d2397
Thabo Lebelo <thabolebelo@icloud.com>
2022-06-17 23:26:47 +0200

    Setup custom domain name

--- lib/on-demand-videos-stack.ts
+++ lib/on-demand-videos-stack.ts
@@ -1,12 +1,15 @@
 import { Construct } from 'constructs';
 import { Bucket } from "aws-cdk-lib/aws-s3"
+import { Certificate, CertificateValidation } from "aws-cdk-lib/aws-certificatemanager";
+import * as route53Targets from "aws-cdk-lib/aws-route53-targets";
+import * as route53 from "aws-cdk-lib/aws-route53";
 import * as origins from "aws-cdk-lib/aws-cloudfront-origins";
 import {
     OriginAccessIdentity, AllowedMethods,
     ViewerProtocolPolicy, Distribution
 } from "aws-cdk-lib/aws-cloudfront";
 import {
-    Stack, StackProps,
+    Stack, StackProps, Duration,
     CfnOutput, RemovalPolicy
 } from 'aws-cdk-lib';
 
@@ -25,6 +28,18 @@ export class OnDemandVideosStack extends Stack {
             comment: "OAI for web application cloudfront distribution",
         });
 
+        /* DNS, DOMAINS, CERTS */
+        // I'm using a domain I own: thabolebelo.com
+        const zone = route53.HostedZone.fromLookup(this, 'HostedZone', {
+            domainName: 'thabolebelo.com'
+        });
+
+        const cert = new Certificate(this, 'Certificate', {
+            domainName: 'videos.thabolebelo.com',
+            subjectAlternativeNames: ['*.videos.thabolebelo.com'],
+            validation: CertificateValidation.fromDns(zone)
+        });
+
         // Creating CloudFront distribution
         const cloudFrontDist = new Distribution(this, "cloudfrontDist", {
             defaultBehavior: {
@@ -34,6 +49,23 @@ export class OnDemandVideosStack extends Stack {
                 allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
                 viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS
             },
+            domainNames: ["videos.thabolebelo.com"],
+            certificate: cert
+        });
+
+        // create DNS record to route traffic to CloudFront
+        const dnsRecord = new route53.ARecord(this, 'videosSubdomain', {
+            zone,
+            recordName: 'videos',
+            target: route53.RecordTarget.fromAlias(
+                new route53Targets.CloudFrontTarget(cloudFrontDist)
+            ),
+            ttl: Duration.seconds(300),
+            comment: 'videos subdomain'
+        });
+
+        new CfnOutput(this, "SubDomain", {
+            value: dnsRecord.domainName
         });
 
         new CfnOutput(this, "Domain", {
