1dae1f50ab6621a6516ce0acd5ee371f788d300e
Thabo Lebelo <thabolebelo@icloud.com>
2022-06-17 19:24:04 +0200

    Code cleanup

--- lib/on-demand-videos-stack.ts
+++ lib/on-demand-videos-stack.ts
@@ -1,16 +1,9 @@
 import { Stack, StackProps } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class OnDemandVideosStack extends Stack {
-  constructor(scope: Construct, id: string, props?: StackProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props?: StackProps) {
+        super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'OnDemandVideosQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+    }
 }
