3de000c78fc5cddbf86bac080f667b6979880961
Thabo Lebelo <thabolebelo@icloud.com>
2022-06-17 23:44:58 +0200

    Stack information

--- bin/on-demand-videos.ts
+++ bin/on-demand-videos.ts
@@ -5,5 +5,7 @@ import { OnDemandVideosStack } from '../lib/on-demand-videos-stack';
 
 const app = new cdk.App();
 new OnDemandVideosStack(app, 'OnDemandVideosStack', {
-  // env: { account: '123456789012', region: 'us-east-1' },
+    stackName: "on-demand-videos",
+    description: "Hosting on-demand streaming video with Amazon S3, Amazon CloudFront, and Amazon Route 53",
+    env: { account: '123456789012', region: 'us-east-1' }
 });
