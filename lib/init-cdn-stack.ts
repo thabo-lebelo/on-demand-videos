209de69b2d9795d42928032b5e13712585d39fab
Dhenain Ambroise <ambroise.dhenain@gmail.com>
2022-08-27 13:21:12 +0200

    More config.json

--- lib/init-cdn-stack.ts
+++ lib/init-cdn-stack.ts
@@ -21,35 +21,36 @@ import * as route53Targets from 'aws-cdk-lib/aws-route53-targets';
 import { Bucket } from 'aws-cdk-lib/aws-s3';
 import { Construct } from 'constructs';
 
+const config = require('../config.json');
+
 export class InitCdnStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
     // Create an S3 bucket
-    const videoBucket = new Bucket(this, 'videoBucket', {
+    const videoBucket = new Bucket(this, config.s3.bucketId, {
       versioned: false,
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
     // Create a CloudFront origin access identity
-    const originAccessIdentity = new OriginAccessIdentity(this, 'cloudfrontOAI', {
-      comment: 'OAI for web application cloudfront distribution',
+    const originAccessIdentity = new OriginAccessIdentity(this, `${config.stack.name}CloudfrontOAI`, {
+      comment: 'Origin Access Identity (OAI) for web application cloudfront distribution',
     });
 
     /* DNS, DOMAINS, CERTS */
-    // I'm using a domain I own: thabolebelo.com
-    const zone = route53.HostedZone.fromLookup(this, 'HostedZone', {
-      domainName: 'thabolebelo.com',
+    const zone = route53.HostedZone.fromLookup(this, `${config.stack.name}HostedZone`, {
+      domainName: config.route53.domainName,
     });
 
-    const cert = new Certificate(this, 'Certificate', {
-      domainName: 'videos.thabolebelo.com',
-      subjectAlternativeNames: ['*.videos.thabolebelo.com'],
+    const cert = new Certificate(this, `${config.stack.name}Certificate`, {
+      domainName: config.customDomain.domainName,
+      subjectAlternativeNames: [config.customDomain.subjectAlternativeNames],
       validation: CertificateValidation.fromDns(zone),
     });
 
     // Creating CloudFront distribution
-    const cloudFrontDist = new Distribution(this, 'cloudfrontDist', {
+    const cloudFrontDist = new Distribution(this, `${config.stack.name}CloudfrontDist`, {
       defaultBehavior: {
         origin: new origins.S3Origin(videoBucket as any, {
           originAccessIdentity: originAccessIdentity as any,
@@ -57,19 +58,19 @@ export class InitCdnStack extends Stack {
         allowedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
         viewerProtocolPolicy: ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      domainNames: ['videos.thabolebelo.com'],
+      domainNames: [config.customDomain.domainName],
       certificate: cert,
     });
 
     // create DNS record to route traffic to CloudFront
-    const dnsRecord = new route53.ARecord(this, 'videosSubdomain', {
+    const dnsRecord = new route53.ARecord(this, `${config.stack.name}Subdomain`, {
       zone,
-      recordName: 'videos',
+      recordName: config.stack.name,
       target: route53.RecordTarget.fromAlias(
         new route53Targets.CloudFrontTarget(cloudFrontDist),
       ),
       ttl: Duration.seconds(300),
-      comment: 'videos subdomain',
+      comment: `${config.stack.name} subdomain`,
     });
 
     new CfnOutput(this, 'SubDomain', {
@@ -83,6 +84,5 @@ export class InitCdnStack extends Stack {
     new CfnOutput(this, 'Bucket', {
       value: videoBucket.bucketName,
     });
-
   }
 }
